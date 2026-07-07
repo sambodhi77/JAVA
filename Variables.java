9f8885fe54cbd2121459e25c5be6ddc2f4cac3bb
Vipul Gaikwad <90903855+Vipul-0722@users.noreply.github.com>
2026-07-07 11:51:03 +0530

    Java programs !

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+
+    public static void main (String[] args){
+        int num1=10;
+        int num2=20;
+        int sum= num1+num2;
+        System.out.print("sum = " + sum);
+    }
+}
---
+++ variables2.java
@@ -0,0 +1,11 @@
+//SWrite a java program to find the reminder when one integer is divded by another.
+
+public class variables2 {
+    public static void main (String[] args){
+        int num1=10;
+        int num2=20;
+        int reminder= num1%num2;
+        System.out.print(reminder);
+    }
+}
+
