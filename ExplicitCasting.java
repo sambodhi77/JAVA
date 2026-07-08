b2420ea64839a29efacf450c30e21efd6a6c809b
Vipul Gaikwad <90903855+Vipul-0722@users.noreply.github.com>
2026-07-08 18:53:49 +0530

    Type Casting Java !

---
+++ ExplicitCasting.java
@@ -0,0 +1,8 @@
+public class ExplicitCasting {
+    public static void main(String[] args) {
+        double a=14.5;
+        int b = (int)a;
+        System.out.println(b);
+    }
+    
+}
---
+++ implicitCasting.java
@@ -0,0 +1,10 @@
+public class implicitCasting {
+    public static void main(String[] args) {
+    int a = 5;
+    double b=a;
+    System.out.print(b);
+
+    }
+    
+    
+}
